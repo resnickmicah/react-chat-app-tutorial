6b467ecf139ec91f62aee2d40beb1062a47da8b9
Micah Resnick <resnickmicah@gmail.com>
2024-08-10 16:48:01 -0400

    Implement socket handlers for user joining
    Send join notification to other users in same room
    Send welcome message to user who joined
    Send user list udpates to UI for all users in room

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
-import express from 'express';
-import http from 'http';
-import cors from 'cors';
-import { Server as SocketServer } from 'socket.io';
+import express from "express";
+import http from "http";
+import cors from "cors";
+import { Server as SocketServer } from "socket.io";
+
+const CHAT_BOT = "ChatBot";
+let chatRoom = "";
+let allUsers = [];
 
 const app = express();
 app.use(cors());
@@ -10,13 +14,48 @@ const server = http.createServer(app);
 const io = new SocketServer(server, {
   cors: {
     // allow connections from client origin
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
-  }
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   console.log(`User connected ${socket.id}`);
-})
 
-server.listen(4000, () => 'Server is running on port 4000');
+  socket.on("join_room", (data) => {
+    const { username, room } = data;
+    console.log(
+      `user ${socket.id} joined room ${room} with username ${username}`
+    );
+    socket.join(room);
+
+    let __createdtime__ = Date.now();
+    // Send notification to all users in room except joiner
+    socket.to(room).emit("receive_message", {
+      message: `${username} has joined the chat room`,
+      username: CHAT_BOT,
+      __createdtime__,
+    });
+
+    // Send welcome message to joiner
+    socket.emit("receive_message", {
+      message: `Welcome ${username}`,
+      username: CHAT_BOT,
+      __createdtime__,
+    });
+
+    // Save list of users in a chat room
+    chatRoom = room;
+    allUsers.push({ id: socket.id, username, room });
+    console.log(
+      "Added socketID, username, room to allUsers:"+
+      `${socket.id}, ${username}, ${room}`
+    );
+    chatRoomUsers = allUsers.filter((user) => user.room === room);
+    socket.to(room).emit("chatroom_users", chatRoomUsers);
+    socket.emit("chatroom_users", chatRoomUsers);
+    console.log(`Updated users list for users in room ${room}`);
+  });
+});
+
+server.listen(4000, () => "Server is running on port 4000");
