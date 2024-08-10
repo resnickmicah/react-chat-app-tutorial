3dafad8a4ee0553b8af199149d53cc1c7f3dda96
Micah Resnick <resnickmicah@gmail.com>
2024-08-10 15:30:20 -0400

    Add joinRoom function

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,13 +1,30 @@
 import styles from "./styles.module.css";
+import { useNavigate } from "react-router-dom";
 
-const Home = () => {
+const Home = ({ username, setUsername, room, setRoom, socket }) => {
+  const navigate = useNavigate();
+  const joinRoom = () => {
+    if (room !== "" && username !== "") {
+      socket.emit("join_room", { username, room });
+    }
+
+    navigate("/chat", { replace: true });
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.formContainer}>
         <h1>{`<>DevRooms</>`}</h1>
-        <input className={styles.input} placeholder="Username..." />
+        <input
+          className={styles.input}
+          placeholder="Username..."
+          onChange={(e) => setUsername(e.target.value)}
+        />
 
-        <select className={styles.input}>
+        <select
+          className={styles.input}
+          onChange={(e) => setRoom(e.target.value)}
+        >
           <option>-- Select Room --</option>
           <option value="javascript">JavaScript</option>
           <option value="node">Node</option>
@@ -15,7 +32,9 @@ const Home = () => {
           <option value="react">React</option>
         </select>
 
-        <button className="btn btnSecondary fullWidth">Join Room</button>
+        <button className="btn btnSecondary fullWidth" onClick={joinRoom}>
+          Join Room
+        </button>
       </div>
     </div>
   );
